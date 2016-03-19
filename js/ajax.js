274dfc5f36ef565602ebb7d374a48368856f4920
Inokenty <fockewulf3@gmail.com>
2016-03-19 19:04:30 +0200

    ajax.js: added arg in done callback

--- js/ajax.js
+++ js/ajax.js
@@ -1,5 +1,8 @@
 // script for performing ajax requests
 
 function ajax(method, uri, action) {
-  $.ajax({ url: uri, method: method }).done(action);
+    $.ajax({ url: uri, method: method })
+    .done(function (data) {
+        action(data);
+    });
 }
